8eae31cfd0c000a5bb328e0fa82a5b62ebeb4950
gonzalo <gonzalosapriza@gmail.com>
2026-01-17 17:44:01 -0500

    Updated Scraper with event, winner, method info

--- ufc_scraper.py
+++ ufc_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 
-def checkFight(fight_url):
+def checkFight(fight_url, name):
     try:
         headers["Referer"] = fighter_url
         res = requests.get(fight_url, headers = headers)
@@ -11,14 +11,38 @@ def checkFight(fight_url):
         doc = BeautifulSoup(res.text, "html.parser")
         stat_tables = doc.find_all(class_ = "b-fight-details__table js-fight-table")
         statistics = []
+        
+        #Get fighters, event, and result
+        event = doc.find(class_ = "b-content__title").text.strip()
+        fighterA, fighterB = doc.find_all(class_ = "b-link b-fight-details__person-link")
+        fighterA = fighterA.text.strip()
+        fighterB = fighterB.text.strip()
+
+        fighterA_S, fighterB_S = doc.find_all(class_ = "b-fight-details__person")
+        if fighterA_S.find('i').text.strip() == "W":
+            winner = fighterA
+        elif fighterB_S.find('i').text.strip() == "W":
+            winner = fighterB
+        else:
+            winner = "DRAW/NC"
+
+        gen_stats = doc.find(class_ = "b-fight-details__content")
+        method = gen_stats.find(class_ = "b-fight-details__text-item_first").find("i", class_ = None).text.strip()
+        rd = gen_stats.find(class_ = "b-fight-details__text-item").text.strip()[-1]
+
+
+
+
+        statistics.append([fighterA, fighterB, event, winner, method, rd])
+        
+
+
         for table in stat_tables:
             rows = table.find_all("tbody")
             new_table = []
             for row in rows:
                 stats = row.find_all(class_ = "b-fight-details__table-text")
-                #stringy = ""
                 i = 0
-                new_row = []
                 fighter1 = []
                 fighter2 = []
                 while i < len(stats):
@@ -30,14 +54,12 @@ def checkFight(fight_url):
                         val2 = f"{val2.split('of')[0].strip()}/{val2.split('of')[1].strip()}"
                     fighter1.append(val1)
                     fighter2.append(val2)
-                    #stringy = stringy + "[" + val1 + ":" + val2 + "] "
                     i+=2
-                new_row.append(fighter1)
-                new_row.append(fighter2)    
-                #print(stringy +"\n")
-                new_table.append(new_row)
+                new_table.append(fighter1)
+                new_table.append(fighter2)
+
             statistics.append(new_table)
-        print(statistics)
+        return statistics
     except requests.exceptions.RequestException as e:
      print(f"Request failed: {e}")
 
@@ -64,7 +86,6 @@ try:
         names = row.find_all(class_ = "b-link b-link_style_black")
         if len(names) != 0:
             if f"{names[0].text} {names[1].text}" == name:
-                #print(f"Success: {names[0].text} {names[1].text} {names[0]['href']}")
                 fighter_url = names[0]['href']
                 break
     ##Will break if fighter DNE in DB
@@ -82,7 +103,10 @@ try:
     for fight in fights:
         opponent = fight.find_all(class_ = "b-link b-link_style_black")[1].text
         print(f"{opponent.strip()}: {fight['data-link']}")
-        checkFight(fight['data-link'])
-        
+        results = checkFight(fight['data-link'], name)
+        print(results)
+######## ADD SOMETHING LATER TO ADD RESULTS TO DB
+
+
 except requests.exceptions.RequestException as e:
     print(f"Request failed: {e}")
